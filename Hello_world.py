a2f6efce649916270bc6520c8460f4f87c9b659a
Dylan L'Heureux <dlheureux018@gmail.com>
2026-05-15 13:33:45 -0400

    Revert "merge"

---
+++ Hello_world.py
@@ -0,0 +1,21 @@
+import random
+print('Hello, world!')
+print('Welcome to Python programming.')
+print('Happy coding!')
+
+
+greetings = [
+    "Hello, Git!",
+    "Greetings, developer!",
+    "Welcome to branching!",
+    "Hi there, coding friend!",
+    "Happy coding!"
+]
+
+
+def get_random_greeting():
+    return random.choice(greetings)
+
+
+print(get_random_greeting())
+print("Learning about branches today!")
